0b7667aeb1744525e0c378995a6afa214f3dc3e8
franciscocaballero <caballerofrancisco22@yahoo.com>
2020-05-24 02:29:45 -0400

    adjusting styling :

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('helloword');
+
+const x = document.querySelector(".x");
+const nav = document.querySelector("nav");
+const burger = document.querySelector(".burger");
+
+burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+});
+
+x.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+});
+
